d5feed859c8ac1147710f6540cdddceb813dda14
Sarita-16 <99497579+Sarita-16@users.noreply.github.com>
2023-07-16 03:01:54 +0530

    complete login page

---
+++ login.js
@@ -0,0 +1,16 @@
+const wrapper = document.querySelector(".wrapper");
+const registerLink = document.querySelector(".register-link");
+const btnPopup = document.querySelector(".btnLogin-popup");
+const iconClose = document.querySelector(".icon-close");
+
+registerLink.addEventListener("click", () => {
+  wrapper.classList.add("active");
+});
+
+btnPopup.addEventListener("click", () => {
+  wrapper.classList.add("active-popup");
+});
+
+iconClose.addEventListener("click", () => {
+  wrapper.classList.remove("active-popup");
+});
